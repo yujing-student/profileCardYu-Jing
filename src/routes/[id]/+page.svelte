3381061d1297c903947d33ef0fb0c8e3984ed490
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-06 11:12:20 +0200

    button weggehaald

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -41,7 +41,6 @@
             </ul>
 
         </section>
-        <button class="button-grid">druk voor meer info</button>
 
 
     </div>
@@ -133,29 +132,6 @@
         margin-top: -1em;
 
     }
-
-    button {
-        box-shadow: 4px 3px 0px 0px;
-        background: #fff;
-        font-weight: normal;
-        height: 44px;
-        width: 13em;
-        border-radius: 0.25rem;
-        padding: 0.5rem 1.5rem;
-        font-size: 1em;
-
-        grid-row: 3;
-        grid-column: 1;
-        display: grid;
-        margin-top: 1em;
-
-
-    }
-
-    button:hover {
-        transform: scale(1.1);
-    }
-
 
     /*ipad en desktop*/
     @media (min-width: 705px) {
